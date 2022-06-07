a1c95f71ee0d60b4304a6d65f9e2a7e5aa69c7db
thaoptbeetsoft <thaopt@beetsoft.com.vn>
2022-06-07 10:04:14 +0700

    format code

--- src/main/java/crm/wealth/management/config/WebSecurityConfig.java
+++ src/main/java/crm/wealth/management/config/WebSecurityConfig.java
@@ -1,7 +1,13 @@
 package crm.wealth.management.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -98,5 +104,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+
+
+	@Value("${cloud.aws.credentials.accessKey}")
+	private String awsId;
+
+	@Value("${cloud.aws.credentials.secretKey}")
+	private String awsKey;
+
+	@Value("${cloud.aws.region.static}")
+	private String region;
+
+	@Bean
+	public AmazonS3 s3client() {
+
+		BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsId, awsKey);
+		AmazonS3 amazonS3Client = AmazonS3ClientBuilder.standard()
+				.withRegion(Regions.fromName(region))
+				.withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+				.build();
+
+		return amazonS3Client;
 	}
 }
